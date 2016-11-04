eabd536166057098bb3fe0e2bc0e16ab2475302f
Andrew Rea <email@andrewrea.co.uk>
2016-11-04 16:48:00 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var exphbs = require('express-handlebars');
+
+var app = express()
+app.engine('.hbs.html', exphbs({defaultLayout: 'layout', extname: '.hbs.html'}));
+app.set('view engine', '.hbs.html');
+
+app.get('/', function(req, res) {
+    res.render('index')
+})
+
+app.listen(3000, '0.0.0.0', function() {
+    console.log('Example app listening on port 3000!')
+})
